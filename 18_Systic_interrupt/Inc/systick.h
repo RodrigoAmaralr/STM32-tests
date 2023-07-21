d3fab6ae23dccf2daba6c1623a33369a463f7572
Rodrigo Amaral <rodrigo.com2@gmail.com>
2023-07-21 20:54:46 +1000

    18_Systic_interrupt

---
+++ 18_Systic_interrupt/Inc/systick.h
@@ -0,0 +1,16 @@
+/*
+ * systick.h
+ *
+ *  Created on: 15 Jul. 2023
+ *      Author: amaral
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void systickDelayMs(int delay);
+void systick_1hz_interrupt(void);
+
+
+
+#endif /* SYSTICK_H_ */
---
+++ 18_Systic_interrupt/Src/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "uart.h"
+#include "adc.h"
+#include "systick.h"
+
+#define GPIOAEN			(1U<<0)
+#define PIN5			(1U<<5)
+
+#define LED_PIN			PIN5
+
+static void systick_calback(void);
+
+int main(void) {
+	/*1.Enable clock access to GPIOA*/
+	RCC->AHB1ENR |= GPIOAEN;
+
+	/*2.Set PA5 as output pin*/
+	GPIOA->MODER |= (1 << 10);
+	GPIOA->MODER &= ~(1 << 11);
+
+	uart2_tx_init();
+	systick_1hz_interrupt();
+
+	while (1) {
+
+	}
+}
+
+static void systick_calback(void) {
+	printf("A second passed !!\n\r");
+	GPIOA->ODR ^= LED_PIN;
+}
+
+void SysTick_Handler(void) {
+	//Do something
+	systick_calback();
+}
+
