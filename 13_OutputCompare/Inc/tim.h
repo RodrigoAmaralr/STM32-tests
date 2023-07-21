1e5912e49ec6ef818917f08eb5e8d7054621128e
Rodrigo Amaral <rodrigo.com2@gmail.com>
2023-07-21 20:52:22 +1000

    13_OutputCompare

---
+++ 13_OutputCompare/Inc/tim.h
@@ -0,0 +1,15 @@
+/*
+ * tim.h
+ *
+ *  Created on: 15 Jul. 2023
+ *      Author: amaral
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+void tim2_1hz_init(void);
+void tim2_pa5_output_compare(void);
+
+#define SR_UIF 		(1U<<0)
+
+#endif /* TIM_H_ */
