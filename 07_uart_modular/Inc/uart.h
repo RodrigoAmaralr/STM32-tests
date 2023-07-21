74c07a4b41cf75774af49551f097cf26ba497ece
Rodrigo Amaral <rodrigo.com2@gmail.com>
2023-07-21 19:50:41 +1000

    07_uart_modular

---
+++ 07_uart_modular/Inc/uart.h
@@ -0,0 +1,17 @@
+/*
+ * uart.h
+ *
+ *  Created on: 13 Jul. 2023
+ *      Author: amaral
+ */
+
+#ifndef UART_H_
+#define UART_H_
+#include "stm32f4xx.h"
+#include <stdint.h>
+
+void uart2_tx_init(void);
+
+
+
+#endif /* UART_H_ */
