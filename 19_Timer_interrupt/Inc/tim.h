326da9fafc98901fd59df2edcf1066e3226613cb
Rodrigo Amaral <rodrigo.com2@gmail.com>
2023-07-21 20:55:00 +1000

    19_Timer_interrupt

---
+++ 19_Timer_interrupt/Inc/tim.h
@@ -0,0 +1,15 @@
+/*
+ * tim.h
+ *
+ *  Created on: 15 Jul. 2023
+ *      Author: amaral
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+void tim2_1hz_init(void);
+
+#define SR_UIF 		(1U<<0)
+void tim2_1hz_Interrupt_init(void);
+
+#endif /* TIM_H_ */
