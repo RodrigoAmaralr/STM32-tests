7d6c8a9c2af0b5c2bc415c50dd174feb8304bd3d
Rodrigo Amaral <rodrigo.com2@gmail.com>
2023-07-23 12:17:04 +1000

    add folder Debug from project 00 to 20

---
+++ 18_Systic_interrupt/Debug/Src/main.d
@@ -0,0 +1,21 @@
+Src/main.o: ../Src/main.c \
+ /mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/core_cm4.h \
+ /mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/cmsis_version.h \
+ /mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ /mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ /mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/mpu_armv7.h \
+ /mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/uart.h ../Inc/adc.h ../Inc/systick.h
+/mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/core_cm4.h:
+/mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/cmsis_version.h:
+/mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/cmsis_compiler.h:
+/mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/cmsis_gcc.h:
+/mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Include/mpu_armv7.h:
+/mnt/HD1TB/GoogleDrive/arq/elt/Microcontrolador/STM32/00\ -\ Embedded\ Systems\ Bare-Metal\ Programming\ Ground\ Up\ -\ STM32/Projects/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/uart.h:
+../Inc/adc.h:
+../Inc/systick.h:
